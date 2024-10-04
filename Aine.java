b5e2899770d117609be6446ba3ed11d8d619c7d1
ABAASAPRIMOS <abaasaprimos@gmail.com>
2024-10-04 16:52:40 +0300

    Add files via upload

---
+++ Aine.java
@@ -0,0 +1,19 @@
+package starter;
+import java.util.Scanner;
+public class {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+int number;
+ //this loop will run until the user enters a negative number
+ do {
+	 System.out.print(Enter a number (negativive to quit): ");"
+	 number = scanner.nextInt();
+	 if (number >=0) {
+		 System.out.printIn("Existing...");
+	 }
+ }while {number >=0);
+	 scanner.close();
+	}}
+
+
